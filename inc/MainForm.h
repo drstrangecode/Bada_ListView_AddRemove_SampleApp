18b1e06ff04ae1564e5f8fbc40920b3d5a0b276c
Dr. Strangecode <drstrangecodephd@gmail.com>
2011-10-20 14:09:11 +0200

    added row icon

--- inc/MainForm.h
+++ inc/MainForm.h
@@ -3,27 +3,65 @@
 
 #include <FBase.h>
 #include <FUi.h>
+#include <FGraphics.h>
+#include <FApp.h>
 
-class MainForm :
-	public Osp::Ui::Controls::Form,
-	public Osp::Ui::IActionEventListener
-{
+class MainForm : public Osp::Ui::Controls::Form,
+        public Osp::Ui::IActionEventListener,
+        public Osp::Ui::Controls::IListViewItemEventListener,
+        public Osp::Ui::Controls::IListViewItemProvider {
 
-// Construction
-public:
-	MainForm(void);
-	virtual ~MainForm(void);
-	bool Initialize(void);
+        // Construction
+    public:
+		MainForm(void);
+        virtual ~MainForm(void);
+        bool Initialize(void);
 
-// Implementation
-protected:
-	static const int ID_BUTTON_OK = 101;
-	Osp::Ui::Controls::Button *__pButtonOk;
+        static const int ID_FORMAT_STRING = 100;
+        static const int ID_FORMAT_BITMAP = 101;
 
-public:
-	virtual result OnInitializing(void);
-	virtual result OnTerminating(void);
-	virtual void OnActionPerformed(const Osp::Ui::Control& source, int actionId);
+        static const int ACTION_ID_ADD_ITEM = 101;
+        static const int ACTION_ID_DELETE_LAST_ITEM = 102;
+
+        // Implementation
+    protected:
+        Osp::Ui::Controls::ListView *pListView;
+
+    public:
+        virtual result OnInitializing(void);
+        virtual result OnTerminating(void);
+
+        // IActionEventListener
+        virtual void OnActionPerformed(const Osp::Ui::Control & source, int actionId);
+
+        // IListViewItemEventListener
+        virtual void
+        OnListViewContextItemStateChanged(Osp::Ui::Controls::ListView &listView,
+                                                 int itemIndex,
+                                                 int elementId,
+                                                 Osp::Ui::Controls::ListContextItemStatus state);
+        virtual void
+        OnListViewItemStateChanged(Osp::Ui::Controls::ListView &listView,
+                                          int itemIndex,
+                                          int elementId,
+                                          Osp::Ui::Controls::ListItemStatus state);
+
+        virtual void OnListViewItemSwept(Osp::Ui::Controls::ListView &listView,
+                                                int itemIndex,
+                                                Osp::Ui::Controls::SweepDirection direction);
+
+        virtual void OnListViewItemLongPressed(Osp::Ui::Controls::ListView & listView,
+                                                      int  itemIndex,
+                                                      int  elementId,
+                                                      bool & invokeListViewItemCallback);
+
+        // IListViewItemProvider
+        virtual int GetItemCount();
+        virtual Osp::Ui::Controls::ListItemBase* CreateItem(int itemIndex,
+                                                            int itemWidth);
+        virtual bool DeleteItem(int itemIndex,
+                                Osp::Ui::Controls::ListItemBase* pItem,
+                                int itemWidth);
 };
 
 #endif	//_MAINFORM_H_
--- src/MainForm.cpp
+++ src/MainForm.cpp
@@ -3,65 +3,105 @@
 using namespace Osp::Base;
 using namespace Osp::Ui;
 using namespace Osp::Ui::Controls;
+using namespace Osp::Graphics;
+using namespace Osp::App;
 
-MainForm::MainForm(void)
-{
+MainForm::MainForm(void) {
 }
 
-MainForm::~MainForm(void)
-{
+MainForm::~MainForm(void) {
 }
 
-bool
-MainForm::Initialize()
-{
-	// Construct an XML form
-	Construct(L"IDF_MAINFORM");
+bool MainForm::Initialize() {
+    // Construct an XML form
+    Construct(L"IDF_MAINFORM");
 
-	return true;
+    return true;
 }
 
-result
-MainForm::OnInitializing(void)
-{
-	result r = E_SUCCESS;
+result MainForm::OnInitializing(void) {
+    result r = E_SUCCESS;
 
-	// TODO: Add your initialization code here
+    pListView = static_cast<ListView *> (GetControl(L"IDC_LISTVIEW"));
+    pListView->SetItemProvider(*this);
+    pListView->AddListViewItemEventListener(*this);
+    pListView->SetSweepEnabled(false);
 
-	// Get a button via resource ID
-	__pButtonOk = static_cast<Button *>(GetControl(L"IDC_BUTTON_OK"));
-	if (__pButtonOk != null)
-	{
-		__pButtonOk->SetActionId(ID_BUTTON_OK);
-		__pButtonOk->AddActionEventListener(*this);
-	}
+    GetFooter()->AddActionEventListener(*this);
 
-	return r;
+    return r;
 }
 
-result
-MainForm::OnTerminating(void)
-{
-	result r = E_SUCCESS;
+result MainForm::OnTerminating(void) {
+    result r = E_SUCCESS;
 
-	// TODO: Add your termination code here
-
-	return r;
+    return r;
 }
 
-void
-MainForm::OnActionPerformed(const Osp::Ui::Control& source, int actionId)
-{
-	switch(actionId)
-	{
-	case ID_BUTTON_OK:
-		{
-			AppLog("OK Button is clicked! \n");
-		}
+// IActionEventListener
+void MainForm::OnActionPerformed(const Control & source, int actionId) {
+	switch (actionId) {
+	case ACTION_ID_ADD_ITEM:
+		pListView->RefreshList(pListView->GetItemCount(), LIST_REFRESH_TYPE_ITEM_ADD);
 		break;
-	default:
+	case ACTION_ID_DELETE_LAST_ITEM:
+		pListView->RefreshList(pListView->GetItemCount()-1, LIST_REFRESH_TYPE_ITEM_REMOVE);
 		break;
 	}
 }
 
+// IListViewItemEventListener
+void MainForm::OnListViewContextItemStateChanged(Osp::Ui::Controls::ListView &listView,
+                                              int itemIndex,
+                                              int elementId,
+                                              Osp::Ui::Controls::ListContextItemStatus state) {
+}
 
+void MainForm::OnListViewItemStateChanged(Osp::Ui::Controls::ListView &listView,
+                                       int itemIndex,
+                                       int elementId,
+                                       Osp::Ui::Controls::ListItemStatus state) {
+}
+
+void MainForm::OnListViewItemSwept(Osp::Ui::Controls::ListView &listView,
+                                int itemIndex,
+                                Osp::Ui::Controls::SweepDirection direction) {
+}
+
+void MainForm::OnListViewItemLongPressed(Osp::Ui::Controls::ListView & listView,
+                                             int  itemIndex,
+                                             int  elementId,
+                                             bool & invokeListViewItemCallback) {
+}
+
+// IListViewItemProvider
+int MainForm::GetItemCount() {
+    return 0;
+}
+
+Osp::Ui::Controls::ListItemBase* MainForm::CreateItem(int itemIndex, int itemWidth) {
+
+    String text;
+    text.Format(100, L"SimpleItem #%d", itemIndex);
+
+	AppResource * res = Application::GetInstance()->GetAppResource();
+	Bitmap * pBitmap = res->GetBitmapN(L"itemicon.png",
+			                           BITMAP_PIXEL_FORMAT_ARGB8888);
+
+	SimpleItem * pItem = new SimpleItem();
+
+	pItem->Construct(Dimension(itemWidth, 100), LIST_ANNEX_STYLE_DETAILED);
+	pItem->SetElement(text, pBitmap);
+
+	delete pBitmap;
+
+    return pItem;
+
+}
+
+bool MainForm::DeleteItem(int itemIndex,
+                       Osp::Ui::Controls::ListItemBase* pItem,
+                       int itemWidth) {
+	/* pItem is not deleted here */
+	return false;
+}
